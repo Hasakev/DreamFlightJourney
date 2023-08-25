66a51f9c997683fda63b24d958a152dfb1ef005a
Kevin <hasakev@gmail.com>
2023-08-26 08:29:01 +1000

    styled homepage, highly subject to change

--- frontend/pages/home-screen.js
+++ frontend/pages/home-screen.js
@@ -1,39 +1,57 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   const [titleText, setTitleText] = useState("Dream Flight Journey");
   const onPressTitle = () => {
-    setTitleText("Dream Flight Journeys");
+    setTitleText("Dream Flight Journeys"); // An example of a state change
   };
   const styles = StyleSheet.create({
     Heading: {
       fontSize: 30,
       fontWeight: "bold",
     },
-    Container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
+    topHalf: {
+      flex: 2,
+      backgroundColor: "#50c3f0",
       justifyContent: "center",
+      alignItems: "center",
+    },
+    bottomHalf: {
+      flex: 3,
+      backgroundColor: "#50c3f0",
+      justifyContent: "start",
+      alignItems: "center",
+    },
+    logo: {
+      width: 132,
+      height: 116,
     },
   });
 
   return (
     console.log("HomeScreen.js: HomeScreen()"),
     (
-      <View style={styles.Container}>
-        <Text style={styles.Heading}> {titleText}</Text>
-        {"\n"}
-        {"\n"}
-        <Button
-          title="Find Itinerary"
-          onPress={() =>
-            navigation.navigate("Profile", {
-              name: "PLACEHOLDER",
-            })
-          }
-        />
+      <View style={{ flex: 1 }}>
+        <View style={styles.topHalf}>
+          <Image
+            style={styles.logo}
+            source={require("../assets/placeholder.png")}
+          />
+        </View>
+        <View style={styles.bottomHalf}>
+          <Text style={styles.Heading}>
+            {titleText} {"\n\n"}
+          </Text>
+          <Button
+            title="Find Itinerary"
+            onPress={() =>
+              navigation.navigate("Profile", {
+                name: "PLACEHOLDER",
+              })
+            }
+          />
+        </View>
       </View>
     )
   );
